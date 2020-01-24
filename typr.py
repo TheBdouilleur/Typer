6e3578c90958b4a0208701c0cc6a8a864c880f21
TheBdouilleur <bdouilleur@gmail.com>
2020-01-24 09:27:30 +0000

    d

--- typr.py
+++ typr.py
@@ -1,27 +1,38 @@
-'''
-Python 3 app
-'''
 import speech_recognition as sr
 from pyautogui import typewrite
 import logger
-text = ""
-
-
-def listen(text):
-    text = text
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("I'm listening bro:")
-        typewrite('Listening...', interval=0.05)
+import requests
+import json
+import ssl
+API_ENDPOINT = 'https://api.wit.ai/speech'
+r = sr.Recognizer()
+m = sr.Microphone()
+ 
+def  listen():# generate str type var from micro input
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    with m as source:
+        print("Listening...")
+        #typewrite('Listening...')
         audio = r.listen(source)
-        text = r.recognize_google(audio, 'fr-FR')
-        return text
-
-def format(text):
-    text = text
+        return audio
+ 
+def recognizeWit(audio_sample): # recognize the text from the audio sample ( in AudioData format)
+    print('Recognizing...')
+    result = r.recognize_wit(audio_sample, 'K3UF6XMKHGPE26SO35I3QSWNSO3J7MMR')
+    text = result.pop('_text')# '_text' is the text key in the 'result' dictionary returned by r.recognize_wit()
+    return text
+ 
+def recognizeHoundify(audio_sample):
+    print('Recognizing...')
+    text = r.recognize_houndify(audio_sample, 'wENbpy4XThJmkSO7u9SdXA==', '9HbD3CujTs-82oqGqUx3GQnu0yE_NXvTe_p6BbO_IfFY10xSgp1K71AJpQcqPwmyMAnF1z-PwTg8Eee7H8vPKA==')
+    return text
+ 
+def format(text):# format the text ( str format ) into a printable and natural-language-like text
+    print("Formatting...")
     checked = False
-    typewrite("Formatting...", interval=0.05)
-    while not checked:
+    while not checked:# treatment of aliases ( ex special characters)
         # 1. Special characters
         if "à" in text:
             text = text.replace("à", "a")
@@ -43,33 +54,33 @@ def format(text):
             text = text.replace("û", "u")
         elif "ç" in text:
             text = text.replace("ç", "c")
-    
+     
         # 2. Punctuation
         elif "virgule" in text:
             text = text.replace("virgule", ", ")
-        elif "point" in text:
-           text = text.replace("point", ". ")
         elif "point d'interrogation" in text:
             text = text.replace("point d'interrogation", "? ")
         elif "point d'exclamation" in text:
             text = text.replace("point d'exclamation", "! ")
+        elif "point" in text:
+           text = text.replace("point", ". ")
         elif "a la ligne" in text:
             text = text.replace("a la ligne", "\n")
+        #TODO. Add correcting support for capitals after punctuation marks
         else:
             checked = True
     return(text)
-
-
+ 
+ 
 if __name__ == "__main__":
     logger.log_start()
     try:
-        text = listen(text)
-        print("You said(raw): {}".format(text))
-        text = format(text)
-        print("You said(refined): {}".format(text))
-        typewrite(text, interval=0.05)
+        audio = listen()
+        rawText = recognizeWit(audio)
+        print("You said(raw): {}".format(rawText))
+        fineText = format(rawText)
+        print("You said(refined): {}".format(fineText))
+        typewrite(fineText)
     except Exception as e:
         logger.log_exception(e)
-        typewrite("Sorry... didn't work"+str(e), interval=0.05)
-
-    
+        typewrite("Sorry... didn't work"+str(e))
